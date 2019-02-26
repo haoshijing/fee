c6b8b778a57f5a06ca983844ac07540ed2f28497
haoshijing <13336151702@163.com>
2019-02-26 10:44:14 +0800

    bugfix

---
+++ src/test/java/com/yingliguoji/fee/test/BaseApiTest.java
@@ -0,0 +1,20 @@
+/*
+ * @(#) TestBase.java 2019-02-26
+ *
+ * Copyright 2019 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.yingliguoji.fee.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author haoshijing
+ * @version 2019-02-26
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.yingliguoji.fee.ApplicationStarter.class, properties = {"spring.profiles.active", "dev"})
+public class BaseApiTest {
+}
---
+++ src/test/java/com/yingliguoji/fee/test/FeeServiceTest.java
@@ -0,0 +1,31 @@
+/*
+ * @(#) FeeServiceTest.java 2019-02-26
+ *
+ * Copyright 2019 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.yingliguoji.fee.test;
+
+import com.yingliguoji.fee.service.FsZcService;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author haoshijing
+ * @version 2019-02-26
+ */
+public class FeeServiceTest extends BaseApiTest {
+
+    @Autowired
+    private FsZcService fsZcService;
+
+
+    @Test
+    public void testFs() {
+        DateTime endDateTime = new DateTime().withTime(0, 0, 0, 0);
+        DateTime startDateTime = endDateTime.plusDays(-1);
+
+        fsZcService.backMoney(startDateTime, endDateTime);
+    }
+}
