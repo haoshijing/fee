b96d7b38dcb1ad7b3958bc0274400efac9d3b74f
郝仕兢 <haoshijing690@163.com>
2019-03-03 13:33:15 +0800

    代码格式优化

--- src/main/java/com/yingliguoji/fee/service/MemberBetService.java
+++ src/main/java/com/yingliguoji/fee/service/MemberBetService.java
@@ -24,65 +24,99 @@ public class MemberBetService {
     private GameRecordService gameRecordService;
 
     public MemberBetResponse queryMemberBetTotal(MemberBetRequest request) {
+        MemberBetResponse response = new MemberBetResponse();
+        List<MemberBetResponse.MemberBetDetailVo> detailVos = obtainDetails(request);
+        response.setMemberBetDetailVoList(detailVos);
+        fillTotalData(response);
+        return response;
+    }
+
+    /**
+     * 查询详情数据
+     *
+     * @param request
+     * @return
+     */
+    private List<MemberBetResponse.MemberBetDetailVo> obtainDetails(MemberBetRequest request) {
         Integer currentAgentId = request.getCurrentAgentId();
         String name = request.getName();
-        List<MemberPo> memberPos =  memberService.getMemberIds(currentAgentId,name);
-        memberPos.add(0, memberService.findById(currentAgentId));
+        List<MemberPo> memberPos = memberService.getMemberIds(currentAgentId, name);
+        MemberPo currentMemberPo = memberService.findById(currentAgentId);
+        if (currentMemberPo != null) {
+            memberPos.add(0, currentMemberPo);
+        }
 
-        List<Integer> memberIds = memberPos.stream().map(
-                memberPo -> memberPo.getId()
-        ).collect(Collectors.toList());
+        List<Integer> memberIds = memberPos.stream().map(memberPo -> memberPo.getId()).collect(Collectors.toList());
 
-        GameRecordPo queryPo = new GameRecordPo();
-        if(request.getStartTime() != null) {
-            queryPo.setStart(new DateTime(request.getStartTime()).toString("yyyy-MM-dd HH:mm:ss"));
-        }
-        if(request.getEndTime() != null) {
-            queryPo.setEnd(new DateTime(request.getEndTime()).toString("yyyy-MM-dd HH:mm:ss"));
-        }
-        if(request.getGameType() != null) {
-            queryPo.setGameType(request.getGameType());
-        }
+        GameRecordPo queryPo = fillQueryPoData(request);
         queryPo.setMemberIds(memberIds);
-
         List<MemberGamePo> memberGamePos = gameRecordService.queryMemberGamePos(queryPo);
 
-        List<MemberBetResponse.MemberBetDetailVo> detailVos = memberGamePos.stream().map(
-                memberGamePo -> {
-                    MemberBetResponse.MemberBetDetailVo detailVo = new MemberBetResponse.MemberBetDetailVo();
-                    detailVo.setBetAmount(memberGamePo.getTotalBetAmount().doubleValue());
-                    detailVo.setValidBetAmount(memberGamePo.getTotalValidBetAmount().doubleValue());
-                    detailVo.setNetAmount(memberGamePo.getTotalNetAmount().doubleValue());
+        List<MemberBetResponse.MemberBetDetailVo> detailVos = memberGamePos.stream().map(memberGamePo -> {
+            MemberBetResponse.MemberBetDetailVo detailVo = new MemberBetResponse.MemberBetDetailVo();
+            detailVo.setBetAmount(memberGamePo.getTotalBetAmount().doubleValue());
+            detailVo.setValidBetAmount(memberGamePo.getTotalValidBetAmount().doubleValue());
+            detailVo.setNetAmount(memberGamePo.getTotalNetAmount().doubleValue());
 
-                    detailVo.setGameType(memberGamePo.getGameType());
-                    detailVo.setGameName(GameTypeUtils.getGameName(memberGamePo.getGameType()));
+            detailVo.setGameType(memberGamePo.getGameType());
+            detailVo.setGameName(GameTypeUtils.getGameName(memberGamePo.getGameType()));
 
-                    Optional<MemberPo> optional = memberPos.stream().filter(memberPo ->
-                    memberPo.getId().equals(memberGamePo.getMemberId())).findFirst();
-                    if(optional.isPresent()){
-                        detailVo.setName(optional.get().getName());
-                    }
-                    return detailVo;
-                }).filter(memberBetDetailVo -> {
+            Optional<MemberPo> optional = memberPos.stream().filter(memberPo -> memberPo.getId().equals(memberGamePo.getMemberId())).findFirst();
+            if (optional.isPresent()) {
+                detailVo.setName(optional.get().getName());
+            }
+            return detailVo;
+        }).filter(memberBetDetailVo -> {
             if (StringUtils.isNotEmpty(request.getName())) {
                 return memberBetDetailVo.getName().contains(request.getName());
             }
             return true;
         }).collect(Collectors.toList());
+        return detailVos;
+    }
+
+    /**
+     * 填充查询参数
+     *
+     * @param request
+     * @return
+     */
+    private GameRecordPo fillQueryPoData(MemberBetRequest request) {
+        GameRecordPo queryPo = new GameRecordPo();
+        Long start = request.getStartTime();
+        Long end = request.getEndTime();
+        if (start != null) {
+            queryPo.setStart(new DateTime(start).toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        if (end != null) {
+            queryPo.setEnd(new DateTime(end).toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        if (request.getGameType() != null) {
+            queryPo.setGameType(request.getGameType());
+        }
+        return queryPo;
+    }
+
+    /**
+     * 填充总数据data
+     *
+     * @param response
+     */
+    private void fillTotalData(MemberBetResponse response) {
         Double totalBetAmount = 0.0;
         Double totalValidBetAmount = 0.0;
         Double totalNetAmount = 0.0;
-        for(MemberBetResponse.MemberBetDetailVo memberBetDetailVo : detailVos){
+        List<MemberBetResponse.MemberBetDetailVo> detailVos = response.getMemberBetDetailVoList();
+        for (MemberBetResponse.MemberBetDetailVo memberBetDetailVo : detailVos) {
             totalBetAmount += memberBetDetailVo.getBetAmount();
             totalValidBetAmount += memberBetDetailVo.getValidBetAmount();
             totalNetAmount += memberBetDetailVo.getNetAmount();
         }
 
-        MemberBetResponse response = new MemberBetResponse();
         response.setMemberBetDetailVoList(detailVos);
         response.setTotalBetAmount(totalBetAmount);
         response.setTotalNetAmount(totalNetAmount);
         response.setTotalValidNetAmount(totalValidBetAmount);
-        return response;
     }
+
 }
